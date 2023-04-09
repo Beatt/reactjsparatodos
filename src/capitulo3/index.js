7abed494009cf5d5590df82458892907ee18729e
Gabriel Jiménez <gio@Gabriels-MacBook-Pro.local>
2023-04-09 13:58:00 -0600

    capitulo 1

---
+++ src/capitulo3/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Capitulo3 = () => {
+  return (
+    <div>
+      <h2>Capitulo 3 - Formulario de inicio de sesión de Google</h2>
+    </div>
+  )
+}
+
+export default Capitulo3
